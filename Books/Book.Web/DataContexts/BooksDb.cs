129bda5c107f9edf466c58286ab2508672664b0f
Pedro Martín <pmargom@gmail.com>
2016-11-30 17:44:57 +0000

    Books

---
+++ Books/Book.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book.Web.DataContexts
+{
+    public class BooksDb
+    {
+    }
+}
---
+++ Books/Book.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book.Web.Startup))]
+namespace Book.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
